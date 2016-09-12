c2617a4df81dd47de4223cce55f35bc90feacadf
nicki6 <nicolai.tesela@gmail.com>
2016-09-12 10:00:52 -0500

    adding dc2010 copy script

---
+++ dc2010/copysf1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#mkdir -p /zdrive/dc2010/sf1/unzipped
+cd /zdrive/dc2010/sf1/unzipped
+#for f in *.zip; do unzip -d unzipped $f; done;
+for i in $(seq -w 01 13);do
+    for f in *000${i}2010.sf1;do
+        echo $f
+#        cat $f | psql -dpropdata -Upostgres -c"copy dc_2010.sf1000${i} from stdin with csv encoding 'latin'";
+    done &
+done;
